ab4d5683226ef18dea2d7bdd350a4d0fe4ce5f4e
ApeX <apex.mbox@gmail.com>
2021-10-23 21:25:37 +0300

    To final review

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -9,10 +9,9 @@ from .locators import BasePageLocators
 
 class BasePage():
 
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url):
         self.browser: WebDriver = browser
         self.url = url
-        #self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
--- Pages/basket_page.py
+++ Pages/basket_page.py
@@ -7,6 +7,4 @@ class BasketPage(BasePage):
         assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Basket should be empty, but it's not."
 
     def should_be_empty_basket_message(self):
-        #basket_message = self.browser.find_element(*BasePageLocators.BASKET_EMPTY_MESSAGE)
-        #assert "Your basket is empty." == self.browser.find_element(*BasePageLocators.BASKET_EMPTY_MESSAGE).text, "Empty basket message is not presented."
-        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY_MESSAGE)
+        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY_MESSAGE), "Empty basket message is not presented."
